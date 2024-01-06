2af4a22b483c73b1564888dbef71c089d5251a76
Animesh shukla <75159346+iamanimeshshukl@users.noreply.github.com>
2024-01-06 23:23:17 +0530

    Add files via upload

---
+++ basic.php
@@ -0,0 +1,14 @@
+<?php 
+
+#PHP print program
+
+echo "Hello PHP";
+print "Hello PHP";
+
+#echo vs print
+
+#echo is fast
+#echo return 0 print return 1
+#print is used to print function large complex expressions
+
+?>
